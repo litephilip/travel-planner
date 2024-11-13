a498b5c963a82528bb0ae0cce9981f87d650da47
Philip Gistedt <philip_91_1@hotmail.com>
2024-11-13 14:01:09 +0100

    refactor: Removed console.logs

--- src/components/Destination/Destination.jsx
+++ src/components/Destination/Destination.jsx
@@ -2,14 +2,13 @@ import PropTypes from 'prop-types';
 import './Destination.css';
 
 function Destination(props) {
-    console.log('props: ', props);
     
     return (
         <div className='destination-container'>
             <li className={`destination-card ${props.destination.isCompleted ? 'completed' : ''}`} onClick={props.toggleComplete}>
                 <p className="destination-name">Plats: { props.destination.name }</p>
                 <p className="destination-activity">Att göra: {props.destination.activity} </p>
-                <span className="remove-button" onClick={(e) => {console.log(e), e.stopPropagation(); props.remove();}}>X</span>
+                <span className="remove-button" onClick={(e) => {e.stopPropagation(); props.remove();}}>X</span>
                 {!props.destination.isCompleted && <p className="done">Klicka för att klarmarkera!</p>}
             </li>
         </div>
